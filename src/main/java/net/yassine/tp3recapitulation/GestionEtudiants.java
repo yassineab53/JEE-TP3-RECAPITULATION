e2fe47777599b1ca18913d697cf715e5d077b12c
yassine4real <yssnabbou2002@gmail.com>
2024-12-01 14:15:42 +0100

    add GestionEtudiants class, and use @Getter annotation for listeEtudiants attribute

---
+++ src/main/java/net/yassine/tp3recapitulation/GestionEtudiants.java
@@ -0,0 +1,10 @@
+package net.yassine.tp3recapitulation;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+public class GestionEtudiants {
+    @Getter
+    public static ArrayList<Etudiant> listeEtudiants = new ArrayList<>();
+}
