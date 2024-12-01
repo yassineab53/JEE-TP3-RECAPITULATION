7e137676e62be99209fe8954f68f31c213c124b8
yassine4real <yssnabbou2002@gmail.com>
2024-12-01 16:09:09 +0100

    add identification.html and Identification Servlet to handle identification

---
+++ src/main/java/net/yassine/tp3recapitulation/Identification.java
@@ -0,0 +1,30 @@
+package net.yassine.tp3recapitulation;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/Identification")
+public class Identification extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+
+        if (login.equals(password)) {
+            RequestDispatcher rd = request.getRequestDispatcher("AffichageEtudiants");
+            rd.forward(request, response);
+        } else {
+            request.setAttribute("login", login);
+            request.setAttribute("password", password);
+            RequestDispatcher rd = request.getRequestDispatcher("UtilisateurNonValide");
+            rd.forward(request, response);
+        }
+    }
+}
