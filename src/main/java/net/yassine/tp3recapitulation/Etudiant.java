d8fa2f81556799468f0714eec4cee3c51064df70
yassine4real <yssnabbou2002@gmail.com>
2024-12-01 14:11:56 +0100

    Override the equals method for cin

--- src/main/java/net/yassine/tp3recapitulation/Etudiant.java
+++ src/main/java/net/yassine/tp3recapitulation/Etudiant.java
@@ -11,4 +11,14 @@ public class Etudiant {
     private String cin,nom;
     private double moyenne;
     private String image;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Etudiant etudiant = (Etudiant) obj;
+        return cin.equals(etudiant.cin);
+    }
 }
+
+
